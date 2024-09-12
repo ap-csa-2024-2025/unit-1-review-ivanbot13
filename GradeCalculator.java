16866932f7baf40f2049fa2c4bcc1f55c5f4d1d1
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-12 10:44:32 -0700

    wip

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,21 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String course = "AP Computer Science A";
+    System.out.println("course name: " + course);
+    int avgTime = 150;
+    int homework1 = 94;
+    int homework2 = 78;
+    int homework3 = 23;
+    int homework4 = 89;
+    double quiz1 = 88.24;
+    double quiz2 = 74.63;
+    double exam = 82.13;
+
+    int hours = avgTime/60;
+    int minutes = avgTime%60;
+    System.out.println("Weekly time spent: " + hours + "hrs and " + minutes + "mins");
+
+
   }
 }
